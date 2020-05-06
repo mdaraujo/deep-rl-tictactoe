2373f0cbfb034b9fc6927d2da1daef5daa21dd9f
Miguel Araújo <mdaraujo@ua.pt>
2020-05-06 20:59:45 +0100

    Changed search params to test input observations

--- train_dqn_minmax.py
+++ train_dqn_minmax.py
@@ -1,11 +1,10 @@
 from stable_baselines import DQN
 
 from gym_tictactoe.envs.tictactoe_env import TicTacToeEnv
-from gym_tictactoe.agents.base import OBS_FORMAT_2D
 from gym_tictactoe.agents.min_max_agent import MinMaxAgent
 from gym_tictactoe.agents.random_agent import RandomAgent
 
-from utils.hyperparams import P_CHAR, REWARDS, N_REPEATS
+from utils.hyperparams import P_CHAR, REWARDS, N_REPEATS, ENV_EXP, NET_ARCH, OBS_FORMAT
 from utils.utils import filter_tf_warnings
 
 from train import train
@@ -17,35 +16,35 @@ ALG = DQN
 
 ENV_AGENT = MinMaxAgent()
 
-OBS_FORMAT = OBS_FORMAT_2D
-
 SELF_PLAY = False
 
-TRAIN_EPISODES = 20000
+TRAIN_EPISODES = 10000
 
-EVAL_FREQ = 2000
+EVAL_FREQ = [1000]
 
 P_CHAR = '-'
 
-GAMMA = [1.0, 0.99]
+GAMMA = [1.0]
 
-ENV_EXP = [0.2]
+# ENV_EXP = [0.2]
 
 N_ENVS = 1
 
-NET_ARCH = [[512, 512], [256, 256, 512], [512, 128, 256]]
+total_trainings = len(OBS_FORMAT) * len(REWARDS) * len(GAMMA) * \
+    len(ENV_EXP) * len(NET_ARCH) * len(EVAL_FREQ) * N_REPEATS
 
-total_trainings = len(REWARDS) * len(GAMMA) * len(ENV_EXP) * len(NET_ARCH) * N_REPEATS
 count = 0
 
-for gamma in GAMMA:
-    for rewards in REWARDS:
-        for env_exp in ENV_EXP:
-            for net_arch in NET_ARCH:
-                for _ in range(N_REPEATS):
+for eval_freq in EVAL_FREQ:
+    for obs_format in OBS_FORMAT:
+        for gamma in GAMMA:
+            for rewards in REWARDS:
+                for env_exp in ENV_EXP:
+                    for net_arch in NET_ARCH:
+                        for _ in range(N_REPEATS):
 
-                    count += 1
-                    print("\n\nTraining {} / {}".format(count, total_trainings))
+                            count += 1
+                            print("\n\nTraining {} / {}".format(count, total_trainings))
 
-                    train(ALG, OBS_FORMAT, ENV_AGENT, SELF_PLAY, TRAIN_EPISODES,
-                          EVAL_FREQ, P_CHAR, gamma, net_arch, rewards, env_exp, N_ENVS)
+                            train(ALG, obs_format, ENV_AGENT, SELF_PLAY, TRAIN_EPISODES,
+                                  eval_freq, P_CHAR, gamma, net_arch, rewards, env_exp, N_ENVS)
--- utils/hyperparams.py
+++ utils/hyperparams.py
@@ -1,5 +1,5 @@
 from gym_tictactoe.envs.tictactoe_env import TicTacToeEnv
-from gym_tictactoe.agents.base import OBS_FORMAT_2D, OBS_FORMAT_ONE_HOT, OBS_FORMAT_RAW
+from gym_tictactoe.agents.base import OBS_FORMAT_RAW, OBS_FORMAT_ONE_HOT, OBS_FORMAT_2D, OBS_FORMAT_2D_FLAT
 from stable_baselines import PPO2, DQN
 
 ALG = [PPO2, DQN]
@@ -10,18 +10,22 @@ SELF_PLAY = [True, False]
 
 base_rewards = [2, 1, 0, -2, -3]
 
-REWARDS = [TicTacToeEnv.DEFAULT_REWARDS, base_rewards]
+REWARDS = [TicTacToeEnv.DEFAULT_REWARDS]
 
-OBS_FORMAT = [OBS_FORMAT_2D, OBS_FORMAT_ONE_HOT, OBS_FORMAT_RAW]
+OBS_FORMAT = [OBS_FORMAT_2D, OBS_FORMAT_2D_FLAT, OBS_FORMAT_ONE_HOT, OBS_FORMAT_RAW]
 
 P_CHAR = '-'
 
 GAMMA = [1.0, 0.99]
 
-ENV_EXP = [0.0, 0.2, 0.5]
+ENV_EXP = [0.2]
+
+# ENV_EXP = [0.0, 0.2, 0.5, 1.0]
+
+NET_ARCH = [[64, 64, 128]]
 
 # NET_ARCH = [[512, 512], [256, 256, 512], [512, 128, 256], [256, 256, 128, 256]]
 
 N_ENVS = [1, 4, 8, 16]
 
-N_REPEATS = 3
+N_REPEATS = 5
