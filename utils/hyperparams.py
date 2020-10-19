0cf820887b8e61f34b3a1dccc8f26ba1586549ba
Miguel Araújo <mdaraujo@ua.pt>
2020-10-19 17:49:14 +0100

    Updated params to test obs formats

--- utils/hyperparams.py
+++ utils/hyperparams.py
@@ -12,7 +12,7 @@ base_rewards = [2, 1, 0, -2, -3]
 
 REWARDS = [TicTacToeEnv.DEFAULT_REWARDS]
 
-OBS_FORMAT = [OBS_FORMAT_2D]
+OBS_FORMAT = [OBS_FORMAT_2D, OBS_FORMAT_2D_FLAT, OBS_FORMAT_ONE_HOT, OBS_FORMAT_RAW]
 
 P_CHAR = '-'
 
@@ -22,13 +22,13 @@ ENV_EXP = [0.2]
 
 # ENV_EXP = [0.0, 0.2, 0.5, 1.0]
 
-NET_ARCH = [[64, 128], [128, 256]]
+NET_ARCH = [[64, 128]]
 
 # NET_ARCH = [[512, 512], [256, 256, 512], [512, 128, 256], [256, 256, 128, 256]]
 
-FILTER_SIZES = [2, 3]
+FILTER_SIZES = [3]
 
-PADS = ['VALID', 'SAME']
+PADS = ['SAME']
 
 N_ENVS = [1, 4, 8, 16]
 
